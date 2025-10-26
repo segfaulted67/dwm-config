3547934eaa7e38f2006985a9f37348dd8ca58fed
larry <segfaulted67@gmail.com>
2025-10-26 16:30:25 +0530

    mod+d rofi

--- config.def.h
+++ config.def.h
@@ -63,7 +63,7 @@ static const char *rofi[] = { "rofi", "-show", "drun", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = rofi } },
+	{ MODKEY,                       XK_d,      spawn,          {.v = rofi } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
