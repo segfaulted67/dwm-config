4a9c5a0e3c23f53d84a7c4ce942155f2124d34fb
larry <segfaulted67@gmail.com>
2025-11-10 21:16:10 +0530

    Added always center patch

--- config.def.h
+++ config.def.h
@@ -70,7 +70,8 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "ghostty", NULL };
+// static const char *termcmd[]  = { "ghostty", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 static const char *rofi[] = { "rofi", "-show", "drun", NULL };
 static const char *maim[]      = { "/bin/sh", "-c", "maim ~/Pictures/Screenshots/screenshot-$(date +%Y%m%d-%H%M%S).png", NULL };
 static const char *maim_full[] = { "/bin/sh", "-c", "maim --select ~/Pictures/Screenshots/screenshot-$(date +%Y%m%d-%H%M%S).png", NULL };
